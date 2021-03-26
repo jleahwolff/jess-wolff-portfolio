8d3c63f08fa4e4fd51fec19f7d17e317b3394446
Jessica Wolff <54365640+jleahwolff@users.noreply.github.com>
2021-03-26 11:49:11 -0500

    reconfig

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-const tailwindcss = require('tailwindcss');
 
 module.exports = {
-    plugins: 
-    [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
-};
+plugins: {
+    tailwindcss: { config: './tailwind.config.js' },
+},
+}
