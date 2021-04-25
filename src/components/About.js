22cf238125b6ebb690285905f933aa20ab6a0edc
Jessica Wolff <54365640+jleahwolff@users.noreply.github.com>
2021-04-25 01:12:17 -0500

    Figuring out Designsecion

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,7 @@ import IllLand from '../assets/illustrated-landing.png';
 import IlluLogo from '../assets/illu-logo.png';
 import NirvanaLogo from '../assets/nirvana-logo.jpeg';
 import NirvanaDash from '../assets/nirvana-dashboard.png';
+import RubSide from '../assets/rubber-side.png';
 
 function About(){
     return(
@@ -18,54 +19,59 @@ function About(){
                 <div id='work-section'/>
                 <div className={'flex justify-center text-left pt-32'}>
                 <a href='https://www.behance.net/gallery/116952765/Rubber-Duck-Finance' target="blank" className={''}>
-                    <div className={'bg-white transform rounded p-5 mx-5 hover:bg-purple-100 transition duration-300 hover:scale-110 flex-1 h-full'}>
-                        <img src={RubberDuck} alt='A dashboard ux design for Rubber Duck Finance.' className={'max-w-sm -mt-24'}/>
-                        <img src={RDLogo} alt='Rubber Duck Finance logo' className={'mt-5 max-w-xs px-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Finances are tough. Understanding them should be easy.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>UX Design, Branding, Front-End</p>
+                    <div className={'bg-white transform rounded p-5 mx-2 hover:bg-purple-100 transition duration-300 hover:scale-95 flex-1 h-full'}>
+                        <img src={RubberDuck} alt='A dashboard ux design for Rubber Duck Finance.' className={'max-w-xs'}/>
+                        <img src={RDLogo} alt='Rubber Duck Finance logo' className={'max-w-xs pr-36 pt-5 pb-2'}/>
+                        <p className={'italic text-sm font-thin max-w-xs mx-auto'}>UX Design, Branding, Front-End</p>
                     </div>
                     </a>
                     <a href='https://www.behance.net/gallery/116725273/SoMe-Social-Media-Strategy' target="blank">
-                    <div className={'transform bg-white rounded p-5 mx-5 hover:bg-purple-100 transition duration-500 hover:scale-110 h-full '}>
-                        <img src={SoMe} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-sm -mt-24'}/>
-                        <img src={SoMeLogo} alt='Social Media Strategy logo' className={'mt-20 max-w-xs pr-32 pl-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Streamline, optimize and track your social media presence.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>User Research, Ux Engineer, Front-end</p>
+                    <div className={'transform bg-white rounded p-5 mx-2 hover:bg-purple-100 transition duration-500 hover:scale-95 h-full '}>
+                        <img src={SoMe} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-xs '}/>
+                        <img src={SoMeLogo} alt='Social Media Strategy logo' className={'max-w-xs pr-44 pt-5 pb-2'}/>
+                        <p className={'italic text-sm font-thin max-w-xs mx-auto'}>User Research, Ux Engineer, Front-end</p>
                     </div>
                     </a>
                     <a href='https://www.behance.net/gallery/117105443/Case-Study-Hell-Creek-Cookers-branding' target="blank">
-                    <div className={'transform bg-white rounded p-5 mx-5 hover:bg-purple-100 transition duration-500 hover:scale-110 h-full '}>
-                        <img src={HCCMag} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-sm -mt-32'}/>
-                        <img src={HCCLogo} alt='Social Media Strategy logo' className={'mt-16 max-w-xs pr-32 pl-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Family owned. Authentic recipes. Culinary-level smoked meats.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>Branding, Logo Design</p>
+                    <div className={'transform bg-white rounded p-5 mx-2 hover:bg-purple-100 transition duration-500 hover:scale-95 h-full '}>
+                        <img src={HCCMag} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-xs'}/>
+                        <img src={HCCLogo} alt='Social Media Strategy logo' className={'max-w-xs pr-44 pt-5 pb-2'}/>
+                        <p className={'italic text-sm font-thin max-w-xs mx-auto'}>Branding, Logo Design</p>
                     </div>
                     </a>
                 </div>
                 {/* SECOND LINE */}
-                <div className={'flex justify-center text-left pt-48'}>
+                <div className={'flex justify-center text-left pt-4'}>
                 <a href='https://www.behance.net/gallery/117671985/Illustrated-Javascript' target="blank">
-                    <div className={'transform bg-white rounded p-5 mx-5 hover:bg-purple-100 transition duration-500 hover:scale-110 h-full '}>
-                        <img src={IllLand} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-sm -mt-32'}/>
-                        <img src={IlluLogo} alt='Social Media Strategy logo' className={'mt-20 max-w-xs pr-32 pl-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Reshape the way you learn.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>UX Design, Animation</p>
+                    <div className={'transform bg-white rounded p-5 mx-2 hover:bg-purple-100 transition duration-500 hover:scale-95 h-full flex flex-col justify-between'}>
+                        <div>
+                        <img src={IllLand} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-xs'}/>
+                        </div>
+                        <div>
+                        <img src={IlluLogo} alt='Social Media Strategy logo' className={'max-w-xs pr-44 pt-5 pb-2'}/>
+                        <p className={'mb-2 italic text-sm font-thin max-w-xs mx-auto'}>UX Design, Animation</p>
+                        </div>
                     </div>
                     </a>
                     <a href='https://www.behance.net/gallery/117864613/Nirvana-Hair-Gallery' target="blank">
-                    <div className={'transform bg-white rounded p-5 mx-5 hover:bg-purple-100 transition duration-500 hover:scale-110 h-full '}>
-                        <img src={NirvanaDash} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-sm -mt-32'}/>
-                        <img src={NirvanaLogo} alt='Social Media Strategy logo' className={'mt-16 max-w-xs pr-32 pl-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Helping a small business out of the HTML dark ages.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>UI Wireframing</p>
+                    <div className={'transform bg-white rounded p-5 mx-2 hover:bg-purple-100 transition duration-500 hover:scale-95 h-full '}>
+                        <img src={NirvanaDash} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-xs'}/>
+                        <div>
+                        <img src={NirvanaLogo} alt='Social Media Strategy logo' className={'max-w-xs pr-52 pt-5 pb-2'}/>
+                        <p className={'mb-2 italic text-sm font-thin max-w-xs mx-auto'}>UI Wireframing</p>
+                        </div>
                     </div>
                     </a>
+
                     <a href='https://www.behance.net/gallery/117671985/Illustrated-Javascript' target="blank">
-                    <div className={'transform bg-white rounded p-5 mx-5 hover:bg-purple-100 transition duration-500 hover:scale-110 h-full '}>
-                        <img src={IllLand} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'max-w-sm -mt-32'}/>
-                        <img src={IlluLogo} alt='Social Media Strategy logo' className={'mt-16 max-w-xs pr-32 pl-10'}/>
-                        <h4 className={'mt-5 mb-2 font-bold text-xl max-w-xs mx-auto'}>Reshape the way you learn.</h4>
-                        <p className={'mb-5 italic text-sm font-thin max-w-xs mx-auto'}>UX Design, Animation</p>
+                    <div className={'w-72 mx-2 flex flex-col justify-between min-h-full bg-white rounded transform hover:bg-purple-100 transition duration-500 hover:scale-95'}>
+                        <div className={'ml-5 mt-5'}>
+                        <img src={IlluLogo} alt='Social Media Strategy logo' className={'max-w-xs pr-44'}/>
+                        <p className={'mt-2 italic text-sm font-thin'}>UX Design, Animation</p>
+                        </div>
+                        <div className={'flex justify-end'}>
+                        <img src={RubSide} alt='A dashboard of SoMe: Social Media Strategy on a Macbook Pro. UX Design' className={'w-9/12'}/>
+                        </div>
                     </div>
                     </a>
                     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,18 +12,11 @@ const [copied, setCopied] = useState(false)
 const emailAddress = 'jessicawolff.me@gmail.com'
 
 return (
-    <div className={'bg-gradient-to-b from-purple-700 to-purple-500 min-h-full flex justify-center items-center flex-wrap'}>
+    <div className={'bg-purple-700 min-h-full flex justify-center items-center flex-wrap'}>
         <div id='contact-section'/>
-        <h1 className={'font-bold px-2 text-7xl text-white w-full text-center py-10'}>Contact.</h1>
-            <div className={'text-xl pb-20'}>
-            <div className={'flex items-center justify-center'}>
-            <a href='https://www.behance.net/jesswolff' target="blank"><img alt='Behance link' className={'p-3'} src={Behance}/></a>
-            <a href='https://www.linkedin.com/in/jwolff2/' target="blank"><img alt='Linkedin link' className={'p-3'} src={Linkedin}/></a>
-            <a href='https://github.com/jleahwolff' target="blank"><img alt='Github link' className={'p-3'} src={Github}/></a>
-            <a href='https://jleahwolff.medium.com/' target="blank"><img alt='Github link' className={'p-3'} src={Medium}/></a>
-            </div>
-            <div className={'flex items-center justify-center pt-5'}>
-            <h5 className={'text-white'}>Send an email to &nbsp;</h5>
+        <h5 className={'font-bold px-2 text-xl text-white w-full text-center pt-10'}>Let's connect and grab a virtual coffee.</h5>
+            <div className={'flex flex-col items-center justify-center text-sm py-5 w-full'}>
+            <h5 className={'text-white'}>Drop a line for business inquiries or to just say hello &nbsp;</h5>
             {
                 copied ? <div className={'font-bold cursor-pointer text-purple-300'}>Copied!</div>
                 :
@@ -31,6 +24,13 @@ return (
                     <div className={'cursor-pointer text-purple-200 underline'}>jessicawolff.me@gmail.com</div>
                 </CopyToClipboard>
             }
+            </div>
+            <div className={'text-xl pb-20'}>
+            <div className={'flex items-center justify-center'}>
+            <a href='https://www.behance.net/jesswolff' target="blank"><img alt='Behance link' className={'p-3'} src={Behance}/></a>
+            <a href='https://www.linkedin.com/in/jwolff2/' target="blank"><img alt='Linkedin link' className={'p-3'} src={Linkedin}/></a>
+            <a href='https://github.com/jleahwolff' target="blank"><img alt='Github link' className={'p-3'} src={Github}/></a>
+            <a href='https://jleahwolff.medium.com/' target="blank"><img alt='Github link' className={'p-3'} src={Medium}/></a>
             </div>
             </div>
     </div>
